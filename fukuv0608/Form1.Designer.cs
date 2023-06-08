26aebc4ae682c92fb65c2a018799b5214209b8c0
arai <130636424+araisousirou1122@users.noreply.github.com>
2023-06-08 16:10:51 +0900

    a

--- fukuv0608/Form1.Designer.cs
+++ fukuv0608/Form1.Designer.cs
@@ -35,11 +35,16 @@
             button4 = new Button();
             label1 = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            button5 = new Button();
+            button6 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(625, 397);
+            button1.Location = new Point(642, 397);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
@@ -49,7 +54,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(625, 426);
+            button2.Location = new Point(642, 426);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 1;
@@ -59,7 +64,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(713, 397);
+            button3.Location = new Point(723, 397);
             button3.Name = "button3";
             button3.Size = new Size(75, 23);
             button3.TabIndex = 2;
@@ -69,7 +74,7 @@
             // 
             // button4
             // 
-            button4.Location = new Point(713, 426);
+            button4.Location = new Point(723, 426);
             button4.Name = "button4";
             button4.Size = new Size(75, 23);
             button4.TabIndex = 3;
@@ -93,11 +98,64 @@
             timer1.Interval = 50;
             timer1.Tick += timer1_Tick_1;
             // 
+            // button5
+            // 
+            button5.Location = new Point(561, 426);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(561, 397);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 6;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(2, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 7;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(171, 97);
+            label3.Name = "label3";
+            label3.Size = new Size(19, 15);
+            label3.TabIndex = 8;
+            label3.Text = "★";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(750, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 9;
+            label4.Text = "label4";
+            label4.Click += label4_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button6);
+            Controls.Add(button5);
             Controls.Add(label1);
             Controls.Add(button4);
             Controls.Add(button3);
@@ -117,5 +175,10 @@
         private Button button4;
         private Label label1;
         private System.Windows.Forms.Timer timer1;
+        private Button button5;
+        private Button button6;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- fukuv0608/Form1.cs
+++ fukuv0608/Form1.cs
@@ -1,12 +1,15 @@
+using Microsoft.VisualBasic.Devices;
 using System.Diagnostics.Eventing.Reader;
 
 namespace fukuv0608
 {
     public partial class Form1 : Form
     {
-        int vx = -5;
-        int vy = -5;
+        int vx = rand.Next(-100,101);
+        int vy = rand.Next(-100,101);
         string chr = "⊂二二二（　＾ω＾）二⊃ﾌﾞｰﾝ";
+        int iTime = 0;
+        static Random rand=new Random();
 
         public Form1()
         {
@@ -40,15 +43,14 @@ namespace fukuv0608
 
         private void button4_Click_1(object sender, EventArgs e)
         {
-            timer1.Interval += 5;
+            timer1.Interval += 15;
         }
 
         private void label1_Click_1(object sender, EventArgs e)
         {
-
-
-
-
+            string t = label1.Text;
+            label1.Text = chr;
+            chr = t;
 
             timer1.Enabled = false;
             label1.Text = "araisoushirou";
@@ -56,6 +58,10 @@ namespace fukuv0608
 
         private void timer1_Tick_1(object sender, EventArgs e)
         {
+
+            iTime++;
+            label4.Text = $"{iTime}";
+
             label1.Left += vx;
             label1.Top += vy;
 
@@ -76,30 +82,63 @@ namespace fukuv0608
 
             if (label1.Top > 433)
             {
-                if (label1.Left > 309)
-                {
-                    vy = -5;
-                    vx = Math.Abs(vx + (vx / 10));
-                    
-                }
 
-
-                
-                
-
+                vy = -5;
             }
 
-            else if (label1.Bottom > ClientSize.Width)
+            Text = $"{MousePosition.X},{MousePosition.Y}";
+
+            var fpos = PointToClient(MousePosition);
+            label1.Text = $"{fpos.X},{fpos.Y}";
+            label2.Left = fpos.X;
+            label2.Top = fpos.Y;
+            label2.Left = fpos.X - label2.Width / 2;
+            label2.Top = fpos.Y - label2.Height / 2;
+
+            //当たり判定
+            if ((fpos.X > label1.Left) && (fpos.X < label1.Right) && (fpos.Y > label1.Top) && (fpos.Y < label1.Bottom))
             {
-                ;
+                timer1.Enabled = false;
+
             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+           
 
 
-            string t = label1.Text;
-            label1.Text = chr;
-            chr = t;
 
         }
+
+
+
+        // 変数mposを宣言して、マウスのフォーム座標を取り出す
+        //// 1. MousePositionにマウス座標のスクリーン左上からのX、Yが入っている
+
+        //label1.Text = $"{MousePosition.X},{MousePosition.Y}";
+
+
+        //// 2. 変数fposを宣言して、PointToClient()でスクリーン座標をフォーム座標に変換
+        // var fpos = PointToClient(MousePosition);
+
+
+        // ラベルに座標を表示
+        //// 変換したフォーム座標は、fpos.X、fpos.Yで取り出せる
+        //label1.Text = $"{fpos.X},{fpos.Y}";
+
+
     }
 }
+
 
