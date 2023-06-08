983f5e9621c53c6137ef86e18f175305d3d15874
arai <130636424+araisousirou1122@users.noreply.github.com>
2023-06-08 13:15:30 +0900

    Initial commit

---
+++ fukuv0608/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace fukuv0608
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(625, 397);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(625, 426);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(713, 397);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(713, 426);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(371, 426);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            label1.Click += label1_Click_1;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ fukuv0608/Form1.cs
@@ -0,0 +1,101 @@
+namespace fukuv0608
+{
+    public partial class Form1 : Form
+    {
+        int vx = -10;
+        int vy = -10;
+        string chr = "(>A<)";
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx = -10;
+            label1.Text = "(.Í.)";
+            label1.ForeColor = Color.Red;
+        }
+
+        
+
+
+
+
+
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            vx = 10;
+            label1.Text = "(>‚`<)";
+            label1.ForeColor = Color.Green;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+            //vx = Math.Abs(vx + (vx / 10));
+            //vx = Math.Abs(vx - (vx / 10));
+            timer1.Interval += -10;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            timer1.Interval += 10;
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+
+
+
+
+            timer1.Enabled = false;
+            label1.Text = "‚¨‘O‚Ì•ê‚¿‚á‚ñ‚Å‚×‚»";
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+               label1.Left += vx;
+                label1.Top += vy;
+
+                if (label1.Left < 0)
+                {
+                    vx = 10;
+                }
+
+                if (label1.Top < 0)
+                {
+                    vy = 10;
+                }
+
+                if (label1.Left > 761)
+                {
+                    vx = -10;
+                }
+
+                if (label1.Top > 433)
+                {
+                    vy = -10;
+                    vx = Math.Abs(vx + (vx / 10));
+                }
+
+                else if (label1.Bottom > ClientSize.Width)
+                {
+                    ;
+                }
+
+
+                string t = label1.Text;
+                label1.Text = chr;
+                chr = t;
+
+            }
+        }
+    }
+
